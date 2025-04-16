6aa0c3b1fb018e7d1d1294836137e59d69adc5c3
Abhay Shriwas <abhay.shriwas@materialplus.io>
2025-04-16 10:36:52 +0530

    First commit - added Glue job script

---
+++ glue_jobs/my_job.py
@@ -0,0 +1,16 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+print("✅ This Glue Job was deployed using GitHub Actions!")
+
+job.commit()
